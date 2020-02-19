84a2c31daafd05990f19d4289d03abe97bb74497
Nishank Kumar <nishank.kumar@chqbook.com>
2020-02-19 16:13:23 +0530

    added routing and drawer

---
+++ chqbook/lib/screens/example1/examplescreen1.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import './components/body.dart';
+
+
+class ExScreen1 extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Home Page")),
+      body: Body(),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
---
+++ chqbook/lib/theme/style.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme() {
+  return ThemeData(
+    primaryColor: Colors.red,
+    accentColor: Colors.orange,
+    hintColor: Colors.white,
+    dividerColor: Colors.white,
+    buttonColor: Colors.red,
+    scaffoldBackgroundColor: Colors.white,
+    canvasColor: Colors.white,
+  );
+}
